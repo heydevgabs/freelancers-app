45825854f6667aec38872b657dc66b201bb5e4e0
heydevgabs <devwithgabs@gmail.com>
2024-10-24 16:20:26 +0200

    initial commit, project scaffolding

---
+++ src/main/java/org/freelance/freelancersservice/FreelancersServiceApplication.java
@@ -0,0 +1,13 @@
+package org.freelance.freelancersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancersServiceApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( FreelancersServiceApplication.class, args );
+    }
+
+}
